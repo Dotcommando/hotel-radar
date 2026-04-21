1dae8a1c4bbfa4a190f4e86b3129c802014de20b
Mikhail Filchushkin <webestet@gmail.com>
2026-04-21 12:52:21 +0300

    Main MongoDB connection added

--- hr-core/src/app.module.ts
+++ hr-core/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GovCyPdfHotelsModule } from './gov-cy-pdf-hotels/gov-cy-pdf-hotels.module';
 import { RawHotelsModule } from './raw-hotels/raw-hotels.module';
 
+const MONGODB_URI = process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/hr-core';
+
 @Module({
-  imports: [GovCyPdfHotelsModule, RawHotelsModule],
+  imports: [
+    MongooseModule.forRoot(MONGODB_URI, {
+      lazyConnection: true,
+      retryAttempts: 0,
+    }),
+    GovCyPdfHotelsModule,
+    RawHotelsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
